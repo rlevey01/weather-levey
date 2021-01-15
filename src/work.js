043c901b5ec042cda0477d47e190629b1a7bac57
Robert Levey <rlevey01@gmail.com>
2021-01-15 09:35:27 -0600

    removed work and t1

--- src/work.js
+++
@@ -1,2 +0,0 @@
-a = { "forecast": "Clear. It is currently 7 degrees outside. It feels like 5 degrees.", "location": "Boston, Massachusetts, United States", "address": "boston" }
-console.log(a.location + '\n forecast =', a.forecast)
